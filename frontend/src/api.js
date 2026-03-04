f9c56b005b33e1cc0bef29ca75dd406a2f10fd1c
MacBook Air M1 <macbookair@MacBooks-MacBook-Air.local>
2026-03-04 12:37:49 +0530

    Fix History schema and frontend API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
+    baseURL: 'https://mahesh-fashion.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
